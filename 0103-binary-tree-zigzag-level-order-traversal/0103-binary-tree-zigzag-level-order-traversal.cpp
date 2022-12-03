98f926a7aca3adf424f3d1d99ac53a4c6b6cdd73
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2022-12-04 04:41:04 +0530

    Time: 4 ms (64.64%), Space: 12 MB (75.79%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,7 +13,7 @@
 /*approach1
    -> PreOrder Traversal of Tree
    -> Reverse level values if level is odd then return
- */
+
 class Solution {
 public:
      vector<vector<int>>ziglevel;
@@ -40,5 +40,75 @@ public:
     }
 };
 
+
 /*
-====================approach1 === PreOrderTraversal ==== End ============*/
+approach1Complexity = {
+       TimeComplexity  =  O(n),
+       SpaceComplexity =  O(n) for Skewed Trees
+};
+*/
+/*====================approach1 === PreOrderTraversal ==== End ============*/
+/*====================approach2 === LevelOrderTraversal ==== Start ============
+approach2
+ ->  push root and NULL into queue
+ -> where NULL represent each level end
+ -> if it's odd level then revese elements 
+ -> append level values into respective level   
+ */
+class Solution {
+public:
+     vector<vector<int>>ziglevel;
+ vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+          if ( !root ){
+               return ziglevel;
+          }
+        queue<TreeNode*>q;
+       q.push(root);
+       q.push(NULL);
+        int i = 0;
+         vector<int>level;
+     while ( q.size() > 1 ) {
+            TreeNode* curNode = q.front();
+             q.pop();
+              if ( curNode ) {
+                level.push_back(curNode->val);  
+                if( curNode->left ){
+                   q.push(curNode->left);
+                }  
+                if ( curNode ->right ) {
+                   q.push( curNode->right );
+                }     
+              } else {
+                   if (i&1) {
+                        reverse (level.begin(),level.end());
+                   }  
+                  ziglevel.push_back(level);
+                     level = {};
+                     i++;
+                  q.push(NULL);
+              }
+     }
+      if(i&1) {
+        reverse(level.begin(),level.end());  
+      }
+      ziglevel.push_back(level);
+   return ziglevel;
+ }
+};
+/*
+approach2Complexity = {
+       TimeComplexity = O(n),
+       SpaceComplexity =O(pow(2,h)-1) for Perfect Binary Tree  
+};
+*/
+/*====================approach2 === LevelOrderTraversal ==== End ============*/
+/*
+ Thank You.
+ Happy Coding!!!.
+ return findHelpFul ? pleaseUpVote : pleaseProvideCommentsForImprovemts; 
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/2874538/C%2B%2B-ororEasy-oror-DFS-oror-BFS
+*/
+
+
+
+
