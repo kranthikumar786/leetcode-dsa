9762ff9bf1d52b8cc3d7379d251a77f91c36142f
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2022-12-04 04:02:42 +0530

    Time: 8 ms (49.56%), Space: 12.6 MB (61.75%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -9,10 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-
-
+/*============approach1 ==== PreOrderTraversal ===========Start============
+approach1 
+       ->  if root is null return;
+       ->  if newlevel then create a newVector of vector to store newly created level
+       ->  each level conatins vector so store values in it:
+       -> move first root->left then root->right
+       -> dO Recursively above all step till all nodes complete
+class Solution {
+public:
+    vector<vector<int>>levelOr;
+    void PreOrder(TreeNode *root , int level ) { 
+            if ( !root ) {
+                   return;
+            }
+            if (levelOr.size() == level ){
+                  levelOr.push_back(vector<int>());
+            }
+            levelOr[level].push_back(root->val);
+            PreOrder(root->left,level+1);
+            PreOrder(root->right,level+1);
+    }    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+            if ( !root ){
+                 return levelOr;
+            }
+        PreOrder(root,0);
+       return levelOr; 
+        }
+};      
+approach1Complexity = {
+       TimeComplexity  = O(n),
+       SpaceComplexity = O(n) for Skewed Tress
+};
+============approach1 ==== PreOrderTraversal ===========End============*/
+/*============approach2 ==== levelOrderTraversal with Two loops ===========Start============
+approach2
+  -> if root null return {};
+  -> push root into queue for further visiting
+  ->get size no of elements that have to visit 
+  -> and explore childrens of every node
+  ->iteratively do above steps till queue becomes empty.
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
@@ -44,8 +81,27 @@ public:
         return levelOr;
     }  
 };      
-        
-/*class Solution {
+/*
+approach2Complexity = {
+       TimeComplexity = O(n),
+       SpaceComplexity =O(pow(2,h)-1) for Perfect Binary Tree
+};
+*/    
+
+
+
+/* ============approach2a ==== levelOrderTraversal with Two loops ===========End============*/
+ 
+
+/* ============approach2a ==== levelOrderTraversal with Single loop===========Start============
+ approach2a
+  -> same as approach2 but with single loop
+  -> push root and NULL into queue  here NULL represent level end
+  -> explore node left childe then right child
+  -> if curNode is NULL add level into levelOr then push NULL into queue and Create newVector with empty size
+  -> do iteratively above all steps till q.size()>1
+*/
+class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>>levelOr;
@@ -77,4 +133,19 @@ public:
        return levelOr;
     }
 };
+/*
+approach2aComplexity = {
+       TimeComplexity =  O(n),
+       SpaceComplexity = O(pow(2,h)-1) for Perfect Binary Tree 
+};
 */
+/* ============approach2a ==== levelOrderTraversal with single loop ===========End============*/
+/*
+Thank You.
+Happy Coding!!!.
+return findHelpFul ? pleaseUpVote : pleaseCommentsForImprovemnts;
+
+https://leetcode.com/problems/binary-tree-level-order-traversal/discuss/2874491/C%2B%2B-ororEASY-oror-DFS-oror-BFS-oror-Single-Loop
+*/
+
+
