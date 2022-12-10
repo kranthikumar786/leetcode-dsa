7f10f93198931706e12c1e10ff1d510b16d008f1
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2022-12-10 17:57:33 +0530

    Time: 428 ms (70.64%), Space: 96 MB (76.16%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -33,18 +33,21 @@ public:
         return ans;     
     }
 };
-/*
 approach1Complexity = {
        SpaceComplexity = O(V+E) + O(V),
        SpaceComplexity = O(V+E)
 };
-*/
+
+approach2  
+  => It is given that i to A[i] and only child hidden infomation :
+  => In adjlist also conaitn same parent and it's first childrens or first connection:
+  => So directly apply traversing on given Vector by trating as adjlist. 
 
 class Solution {
 public:
   int longestCycle(vector<int>& edges) {
     int n = edges.size(),res = -1;
-    vector<pair<int,int>>distArray(n,{-1,-1});
+      vector<pair<int,int>>distArray(n,{-1,-1});
     for (int i = 0 ; i < n ; i++ ) {
         for (int  j = i , dist = 0 ; j != -1 ; j = edges[j]) {
           auto [dist_i,from_i] = distArray[j];
@@ -62,17 +65,59 @@ public:
   }
   };
 
-/*
+
 approach2Complexity = {
        TimeComplexity = O(V),
        SpaceComplexity = O(V)
 };
 
-*/
-  /*
   imp points:
 1) approach2 you can't apply for normal find cycle in directed or undirected graph until if sure only one child for one parent:
 2) This Question conating only one child one parent 
 3) some time you don't need to convert given input into adjlist or adjMatrxi be clear with input format 
 4)  
   */
+class Solution {
+public:
+  int longestCycle(vector<int>& edges) {
+      int n = edges.size(),res = -1;
+      vector<int>indegree(n),vis(n);
+      queue<int>q;
+      for (int i = 0 ; i < n ;i++ ){
+          if ( edges[i] != -1)
+              indegree[edges[i]]++;
+      }
+      for(int i=0;i<n ;i++){
+          if ( indegree[i] == 0 )
+              q.push(i);
+      }
+      while ( !q.empty() ) {
+          int top = q.front();
+          vis[top] = 1;
+          q.pop(); 
+          if ( edges[top] != -1 ) {    
+              if ( --indegree[edges[top]] == 0 ) {
+                  q.push(edges[top]);   
+              }
+          } 
+      }
+      for ( int i = 0 ; i < n ; i++) {
+          if ( !vis[i] ) {
+              int cnt = 0;
+              int start = i;
+              int cur = edges[i];
+              vis[i] = 1;
+              while ( start != cur ) {
+                  if (indegree[cur] == 0) {
+                      break;
+                  }
+                  cnt++;
+                  cur = edges[cur];
+                  vis[cur] = 1;
+              }
+              res = max (res, cnt+1);
+          }
+      }
+      return res;
+  }
+}; 
