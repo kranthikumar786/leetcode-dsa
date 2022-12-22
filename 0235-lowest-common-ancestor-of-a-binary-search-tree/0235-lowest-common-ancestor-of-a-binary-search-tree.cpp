ca04781c42f6e11c2281976177ab120dd4a7a11a
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2022-12-22 07:44:55 +0530

    Time: 33 ms (87.27%), Space: 23.3 MB (70.32%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,7 +11,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-       /*if ( !root || root == p || root == q) {
+       
+        /*Approach 1:
+                   Normal Recursion Traversal with cases : 
+                     if either of p or q found in left or right  return left or right :
+
+                      the moment both left and right are returning not null means found LCA
+       */
+        /*if ( !root || root == p || root == q) {
             return root;
        }
         
@@ -19,6 +26,11 @@ public:
         TreeNode* right = lowestCommonAncestor(root->right,p,q);
         return !left ? right : !right ? left : root;
         
+        */
+        
+        /*
+        Approach 2:
+            Given input is BST so it is having relation with it's subtrees to move based on condtion so we will use that point and find LCA  using recusrion method
         */
         
         if ( !root || root == p || root == q) {
@@ -34,8 +46,10 @@ public:
         }
         return root;
     
-    //}
+    
       /*  
+      approach2 logic using iterative method
+      
        TreeNode * cur = root;
          while ( cur ){
              if (cur->val > p->val && cur->val > q->val) {
@@ -49,6 +63,29 @@ public:
              }
          }
         return cur;
-        */
+    
+       
+     
+       approach1Complexity = {
+                 TimeComplexity = O(n),
+                 SpaceComplexity =O(H)
+       
+       };
+       approach2Complexity = {
+                 TimeComplexity = O(logn),
+                 SpaceComplexity =O(H)         
+       };
+       approach3Complexity = {
+                 TimeComplexity = O(logn),
+                 SpaceComplexity =O(1)       
+       };
+       
+       Note : for approach1 & 2 Stack Space Considered!
+        
+       Thank You.
+       Happy Coding!!!
+        return findHelpFul ? pleaseUpvote : pleaseProvideCommentsForImprovements;   
+       https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/discuss/2937331/C%2B%2BororEasy-oror-3-approaches/ 
+       */
     }
 };
