ee86de86491b35dd99ad8504938e8cb3763b428c
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2022-12-27 00:08:08 +0530

    Time: 43 ms (77.80%), Space: 32.8 MB (43.69%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+if ( !root){
+             return NULL;
+           }
+         root->left  = deleteNode(root->left,key);
+         root->right = deleteNode(root->right,key);
+            if ( root->val == key && root->right ) {
+                     return root->right;
+          }
+          if ( root->val == key && root->left ) {
+                   return root->left;
+          }
+        if ( root->val == key && !root->left && !root->right ){
+                     return NULL;
+          }
+        return root; 
+
+ Problem with PostOrder traverrsal in this problem is :
+  for example 1: 
+  3 and it's childrens are deleting.
+  even not correct way for BST s
+Correct way is to find what is correct place to attach it's left child is imp Point Here.
+*/
+class Solution {
+public:
+     TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root) 
+            if(key < root->val) root->left = deleteNode(root->left, key);    
+            else if(key > root->val) root->right = deleteNode(root->right, key);       
+            else {
+                if(!root->left && !root->right) return NULL;          
+                if (!root->left || !root->right)
+                    return root->left ? root->left : root->right;                                              
+                TreeNode* t = root->left;                        
+                while( t->right ) 
+                      t = t->right;     
+                root->val = t->val;                            
+                root->left = deleteNode(root->left, t->val);
+            }
+        return root;
+    }   };
