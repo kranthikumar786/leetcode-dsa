82d1a88b078d5011170f577b14239a11c10afc46
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2023-11-28 22:34:25 +0530

    Time: 15 ms (76.62%), Space: 12.5 MB (57.28%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div583-delete-operation-for-two-strings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,55 @@
+/*class Solution {
+public:
+    int minDistance(string word1, string word2) {      
+        /*
+         "sea" i 
+         "eat" j 
+
+ if( i ==ended) {
+       retrun j remainging length; 
+ }
+  if( j == ended) {
+        retrun i remainign length;
+  }
+
+A[i] == A[j] {
+     return 0 + r(i+1, j+1);
+  retrun 1+  min (r (i+1,j) , r(i,j+1));     
+}
+
+ s -> e 
+ 
+
+        */
+
+
+
+     class Solution {
+public:
+    int minDistance(string word1, string word2) {
+     
+        int m = word1.size();
+        int n = word2.size();
+     vector<vector<int>>DP(m + 1,vector<int>(n + 1));
+        
+        for(int i = 0 ; i<=m ; i++) {
+            DP[i][0] = i;
+        }
+        for (int j = 0 ; j <= n ;j++) {
+            DP[0][j] = j;
+        }
+        
+       for ( int i = 1 ; i <= m ; i++) {
+            for (int j = 1; j <= n ;j++) {
+                if(word1[i - 1] == word2[j - 1]){
+                     DP[i][j] = DP[i-1][j-1];
+                }
+                else {
+                    DP[i][j] = min(DP[i-1][j] , DP[i][j-1])+1;
+                }
+            }
+       }  
+      return DP[m][n];  
+    }
+};   
+    
