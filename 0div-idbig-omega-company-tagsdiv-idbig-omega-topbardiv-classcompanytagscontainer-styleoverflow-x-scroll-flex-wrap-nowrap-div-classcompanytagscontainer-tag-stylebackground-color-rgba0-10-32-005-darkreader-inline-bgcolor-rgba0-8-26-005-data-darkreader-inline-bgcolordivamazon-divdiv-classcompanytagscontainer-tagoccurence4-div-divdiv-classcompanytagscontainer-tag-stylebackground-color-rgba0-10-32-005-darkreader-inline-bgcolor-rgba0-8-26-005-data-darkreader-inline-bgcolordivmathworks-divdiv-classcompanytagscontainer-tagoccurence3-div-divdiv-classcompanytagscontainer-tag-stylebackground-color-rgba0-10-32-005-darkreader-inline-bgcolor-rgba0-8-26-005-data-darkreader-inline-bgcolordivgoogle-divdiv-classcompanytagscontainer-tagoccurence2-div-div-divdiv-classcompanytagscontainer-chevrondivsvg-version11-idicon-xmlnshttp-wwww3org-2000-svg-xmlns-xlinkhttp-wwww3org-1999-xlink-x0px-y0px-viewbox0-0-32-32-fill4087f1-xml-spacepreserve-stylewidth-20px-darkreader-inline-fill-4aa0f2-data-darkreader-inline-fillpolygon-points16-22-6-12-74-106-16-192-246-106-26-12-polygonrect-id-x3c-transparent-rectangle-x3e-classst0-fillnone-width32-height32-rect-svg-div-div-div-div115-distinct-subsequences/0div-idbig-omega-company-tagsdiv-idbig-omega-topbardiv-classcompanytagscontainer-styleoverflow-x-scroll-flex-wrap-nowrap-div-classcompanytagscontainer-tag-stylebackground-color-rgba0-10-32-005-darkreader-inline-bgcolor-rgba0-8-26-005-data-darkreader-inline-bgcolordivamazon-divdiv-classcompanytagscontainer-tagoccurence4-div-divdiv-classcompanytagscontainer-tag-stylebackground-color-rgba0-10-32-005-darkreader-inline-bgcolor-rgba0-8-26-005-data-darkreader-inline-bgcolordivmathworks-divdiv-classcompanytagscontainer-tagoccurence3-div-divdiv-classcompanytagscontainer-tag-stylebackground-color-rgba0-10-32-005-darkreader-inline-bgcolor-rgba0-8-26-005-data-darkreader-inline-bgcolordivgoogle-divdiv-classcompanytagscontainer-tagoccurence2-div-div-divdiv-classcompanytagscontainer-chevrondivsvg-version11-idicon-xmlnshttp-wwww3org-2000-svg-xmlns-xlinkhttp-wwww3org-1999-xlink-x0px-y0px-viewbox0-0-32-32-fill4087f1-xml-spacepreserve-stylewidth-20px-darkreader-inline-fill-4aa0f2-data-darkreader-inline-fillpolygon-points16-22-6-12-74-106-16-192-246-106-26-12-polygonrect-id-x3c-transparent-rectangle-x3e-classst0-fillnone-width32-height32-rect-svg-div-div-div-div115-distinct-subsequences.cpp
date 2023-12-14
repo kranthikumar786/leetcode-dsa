59306337d99b85e8b5b9da7a4c5b8b290a28fadb
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2023-12-14 22:56:20 +0530

    Time: 1692 ms (9.44%), Space: 422.7 MB (6.54%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+  int numberOfDistinctSubsequences(int indexi, int indexj, string s, string t, vector<vector<int>>& dp) {
+  
+    if( indexj < 0 ) {
+         return 1;
+    }
+    if( indexi < 0 ) {
+        return 0;
+    }
+
+      if(dp[indexi][indexj] != -1) {
+          return dp[indexi][indexj];
+      }
+
+      
+    if(s[indexi] == t[indexj]) {
+        return dp[indexi][indexj] =  numberOfDistinctSubsequences(indexi -1 ,indexj -1, s, t, dp) + numberOfDistinctSubsequences(indexi - 1 , indexj ,s, t, dp);
+    }
+    return  dp[indexi][indexj] = numberOfDistinctSubsequences(indexi - 1 ,indexj ,s, t, dp);
+
+  }
+
+    int numDistinct(string s, string t) {
+        int n = s.size()-1;
+        int m = t.size()-1;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return numberOfDistinctSubsequences(n, m, s, t, dp);
+    }
+};
+
+/*
+
+Time Limit Exceeded
+50 / 65 testcases passed
+*/
