22bda3b623c0fff3f301e101db449e5d9c7e58ce
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2023-12-14 19:34:43 +0530

    Time: 7 ms (97.24%), Space: 11.2 MB (88.55%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1092-shortest-common-supersequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1092-shortest-common-supersequence.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        
+    int m = str1.size();
+    int n = str2.size();
+
+    int dp[m+1][n+1];
+
+    for(int i = 0 ; i <= m ; i++){
+        dp[i][0] = 0;
+    }
+    for(int j = 0 ; j <= n ; j++) {
+        dp[0][j] = 0;
+    }
+
+
+for(int i = 1; i <= m ; i++) {
+    for(int j = 1 ; j<= n ;j++) {
+        if(str1[i - 1] == str2[j - 1]) {
+            dp[i][j] = 1 + dp[i-1][j-1]; 
+        } else {
+            dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+        }
+    }
+ }
+
+int  i = m , j = n;
+ string ans = ""; 
+ 
+ while(i > 0 && j > 0) {
+       
+       if(str1[i-1] == str2[j-1]) {
+           ans += str1[i-1];
+           i--;j--;
+       }
+       else if(dp[i-1][j] > dp[i][j-1]) {
+            ans += str1[i-1];
+            i--;
+       }
+       else{
+           ans += str2[j-1];
+           j--;
+       }
+ }
+ cout<<ans;
+ while(i > 0) {
+     ans += str1[i-1];
+     i--;
+ }
+ while(j > 0) {
+     ans += str2[j-1];
+     j--;
+ }
+ reverse(ans.begin() , ans.end());
+ return ans;
+    }
+};
