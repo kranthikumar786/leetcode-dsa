2b8335e4c6309b20fa15a14bdb547fffdeef2f2a
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2023-12-23 01:37:12 +0530

    Time: 42 ms (70.26%), Space: 28.9 MB (33.12%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1312-minimum-insertion-steps-to-make-a-string-palindrome/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1312-minimum-insertion-steps-to-make-a-string-palindrome/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int palindrom(int left , int right, string& s , vector<vector<int>>&DP) {
+/*int palindrom(int left , int right, string& s , vector<vector<int>>&DP) {
      if(right <= left) { return 0;}
      if(DP[left][right] != -1) { return DP[left][right];}
      if(s[left] == s[right]) {
@@ -11,6 +11,9 @@ int palindrom(int left , int right, string& s , vector<vector<int>>&DP) {
     return DP[left][right] =  min (palindrom(left ,right - 1 ,s, DP) + 1 , palindrom(left + 1 ,right , s, DP) + 1);
     
     
+    
+}
+
    /*if(s[left] == s[right-1]) {
          return  palindrom(left ,right - 1 ,s) + 1;
   }
@@ -18,13 +21,30 @@ int palindrom(int left , int right, string& s , vector<vector<int>>&DP) {
          return palindrom(left + 1 ,right , s) + 1 ; 
     }
 return palindrom(left + 1 , right -1 ,s)+2;
-*/
-}
+
+}*/
     int minInsertions(string s) {
   
-        int n = s.length();
-        vector<vector<int>>DP(n+1, vector<int>(n+1 , -1));
-        return palindrom(0, n-1,s,DP);
+       int n = s.length();
+       vector<vector<int>>DP(n+1, vector<int>(n+1 , 0));
+       // return palindrom(0, n-1,s,DP);
+    
+        
+        for (int len = 2; len <= n; ++len) {
+        for (int left = 0; left <= n - len; ++left) {
+            int right = left + len - 1;
+
+            if (s[left] == s[right]) {
+                DP[left][right] = DP[left + 1][right - 1];
+            } else {
+                DP[left][right] = min(DP[left][right - 1] + 1, DP[left + 1][right] + 1);
+            }
+        }
+            
+    }
+        return DP[0][n - 1];
+
+    
     }
 };
 
