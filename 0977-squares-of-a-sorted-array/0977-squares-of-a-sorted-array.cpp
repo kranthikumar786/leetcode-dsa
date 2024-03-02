78342e6a59cf4ca9332c73e2a85c7e23b3bf173c
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2024-03-02 12:38:12 +0530

    Time: 18 ms (86.47%), Space: 29.3 MB (42.10%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        int minIndex = 0 , minValue = 10001;
+      for (int i = 0 ; i < n ; i++) {   
+          if(abs(nums[i]) < abs(minValue)) {
+              minValue = nums[i];
+              minIndex = i;
+          }
+          
+          if(abs(nums[i]) > abs(minValue)){
+               break;
+          }
+      }    
+        //cout<<minIndex<<minValue;
+     int leftPointer = minIndex , rightPointer = minIndex + 1 ;
+     while (leftPointer >= 0 && rightPointer < n) {
+            
+           if(abs(nums[leftPointer]) < abs(nums[rightPointer])) {
+                   ans.push_back(nums[leftPointer] * nums[leftPointer]);    
+                leftPointer--;
+           } else {
+               ans.push_back(nums[rightPointer] * nums[rightPointer]);
+               rightPointer++;   
+           }
+     }
+       while(leftPointer >=0 )  {
+            ans.push_back(nums[leftPointer] * nums[leftPointer]);
+            leftPointer--;
+       }
+       
+        while (rightPointer < n ) {
+            ans.push_back(nums[rightPointer] * nums[rightPointer]);
+            rightPointer++;
+        }
+      return ans;  
+    }
+};
+
+/*
+Dry RUN : IS DONE 
+Test cases : 
+
+[-4,-1,0,3,10] = pass : given test case:
+[-7,-3,2,3,11] = > pass : given test case : 
+
+[1,2,3,4,5] = > pass : increaing order 
+[1,1,1,2,2] = > non-decrasing order 
+[5,4,4,3,3] = > pass non-incraing order  invalid test case : 
+[]
+
+
+*/
