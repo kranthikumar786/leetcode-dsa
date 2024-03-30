e996974aea6240c28f99b5193285bb2775a9b5ca
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2024-03-31 00:21:00 +0530

    Time: 203 ms (48.09%), Space: 76.1 MB (92.34%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2580-count-ways-to-group-overlapping-ranges/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2580-count-ways-to-group-overlapping-ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+     int mod = 1e9+7;
+     long long pow(long long m, long long n) {
+        if (n == 0) return 1;
+        return (((n % 2) ? m : 1) * pow(m * m % mod, n / 2)) % mod;
+    }
+
+    int countWays(vector<vector<int>>& ranges) {
+     
+     sort(ranges.begin(),ranges.end());
+      
+        int cnt = 1 ;
+        auto r = ranges[0];
+        for(int i = 0 ; i < ranges.size(); i++) {
+            
+            if(r[1] >= ranges[i][0]){ // Overlapping confimred
+               r[1] = max(r[1],ranges[i][1]); 
+            }else{
+                r = ranges[i], ++cnt; 
+            }
+        }
+    return pow(2, cnt);     
+    }
+};
