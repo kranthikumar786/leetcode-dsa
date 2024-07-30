801d13cb3a050e74a86374d634cade606cb0b705
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2024-07-30 07:12:12 +0530

    Time: 106 ms (37.00%), Space: 24.4 MB (57.26%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1653-minimum-deletions-to-make-string-balanced/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+        stack<char> charStack;
+        int deletedCount = 0;
+        
+        for(int i = 0 ; i < n ; i++) {
+            if ( !charStack.empty() && charStack.top() == 'b' && s[i] == 'a') {
+                     charStack.pop();
+                      deletedCount++;   
+            } else {
+                charStack.push(s[i]);
+            }
+        }
+        return deletedCount;
+    }
+};
