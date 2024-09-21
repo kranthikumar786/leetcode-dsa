839336ff6718fa0c2b561d344425b63a898b392a
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2024-09-21 20:58:51 +0530

    Time: 0 ms (100.00%), Space: 14.2 MB (49.01%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div386-lexicographical-numbers/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div386-lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void help(int i, vector<int>& ans, int n) {
+        if (i > n)
+            return;
+
+        ans.push_back(i);
+
+        help(i * 10, ans, n);
+
+        if (i % 10 != 9)
+            help(i + 1, ans, n);
+    }
+
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        help(1, ans, n);
+        return ans;
+    }
+};
