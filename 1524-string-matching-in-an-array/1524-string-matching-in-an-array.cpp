55f51fbabc4f20d07f7399a566285409620ee257
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-01-04 01:31:53 +0530

    Time: 3 ms (45.7%), Space: 11.9 MB (22.95%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -18,14 +18,13 @@ bool isSubString(string text , string pattern){
        vector<string>ans;
        int wordsLength = words.size();
         for(int lWordIndex = 0 ;lWordIndex < wordsLength  ; lWordIndex++) {
-              string lWord = words[lWordIndex];
              for(int rWordIndex = 0 ; rWordIndex < wordsLength; rWordIndex++ ){
-                   if(rWordIndex != lWordIndex && lWord.length() <= words[rWordIndex].length()){
+                   if(rWordIndex != lWordIndex && words[lWordIndex].length() <= words[rWordIndex].length()){  // avoid to match substring if defintely not found and self match scenarioes
                          //cout<<words[rWordIndex]<<lWord<<"\n";
-                         if(isSubString(words[rWordIndex],lWord)){
+                         if(isSubString(words[rWordIndex],words[lWordIndex])){
                           //   cout<<lWord<<" "<<lWordIndex<<" "<<rWordIndex<<"\n";
-                            ans.push_back(lWord);
-                             break;
+                            ans.push_back(words[lWordIndex]);
+                             break; // duplicates are not allowed push only once substring found
                          }
                    }
              }  
@@ -33,3 +32,20 @@ bool isSubString(string text , string pattern){
      return ans;
     }
 };
+
+
+/*
+ timeComplexity : O(wordsLength * wordsLength * maxWordSize * maxWordSize) : 100 * 100 * 30 * 30 = 10^4 * 10^2 ~ 10^6 : works without TLE :
+spaceComplexity : O(1) for lWord 
+
+------------------------------------
+Input : ["as","as","as","as","as"] :
+Invalid Test case  : expected variables words[0]:'as' and words[1]:'as' to have different values
+-------------------------------------------------------
+Input : ["as","aas","bas","cas","das"] : 
+A/O :    ["as"]
+E/O : ["as"]
+
+
+
+*/
