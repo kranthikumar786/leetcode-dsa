bef957039d618bd27fd780feb2e26052a0473c5c
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-01-14 07:24:15 +0530

    Time: 0 ms (100%), Space: 97.4 MB (22.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int ans = 0 , n = prices.size() ;
+         int profit = 0 , n = prices.size() ;
            if( n == 1) { return 0;}
-          vector<int>suffixMax(n, 0) ;
+           int left = 0 , right = 1 , max1 = prices[0];
+               while(right  < n) {
+                if(prices[left] > prices[right]){
+                   profit = max(profit , max1 - prices[left]) ;
+                   max1 = prices[right];
+                   left = right;
+                 }
+                if(prices[right] > max1) {
+                   max1 =  prices[right];
+                  }
+               right++;    
+               } 
+profit = max(profit , max1 - prices[left]) ;
+         return profit;
+    }
+};
+
+
+/*
+
+
+ vector<int>suffixMax(n, 0) ;
          suffixMax[n-1] = prices[n-1];
            int  j = n-2 ;
             while(j >= 0) {
@@ -12,10 +33,57 @@ public:
             }
             for(int i = 0 ; i < n ;i++) {
                  ans = max(ans , suffixMax[i] - prices[i]);  
-            } 
-         return ans;
-    }
-};
+            }
+  1, 2,200, 4,5,6,7,300 : 
+ 
+
+ 
+ 
+ 20 , 1, 200 , 300, 400, 5,600 : 
+ 
+ 20 , ....................... 600  looking for max : but meanwhile found min  then update:
+   profit till now : 
+    update min too 
+    update max tooo :  
+
+  int profit = 0 ; 
+  int leftptr = 0 . rightPtr = 1 , max1 = 0; 
+   
+
+
+      found min value : 
+
+
+ p = 0 , max1 = a[0]; 
+ l = 0 , r = 1 ;
+20 , 1, 200 , 300, 400, 5,600 : 
+
+20 , 10, 200 , 300, 400, 5, 600,700 :
+
+
+
+       while ( right < n) {
+         if(nums[left] > nums[right]){
+       profit = max(profit , max1 - nums[left]) ;
+       max1 = nums[right];
+       left = right;
+       }
+     if(nums[right] > max1) {
+        max1=  nums[right];
+     }
+      right++;
+}
+
+    profit = max(profit , max1 - nums[left])
+
+
+ 
+
+     
+
+
+*/
+
 
    /*
         
