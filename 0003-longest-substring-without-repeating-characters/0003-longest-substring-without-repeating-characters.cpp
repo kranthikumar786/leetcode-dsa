f8e837a579879c15976914da2653f8728219fc12
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-01-14 05:37:21 +0530

    Time: 19 ms (33.09%), Space: 16.9 MB (12.31%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    set<char>se;
+  int leftPtr = 0 ,rightPtr = 0,n = s.length();
+   if( n == 0) { return 0;}
+   int ans = 0; 
+     while(rightPtr < n){
+        while(leftPtr < rightPtr && se.size() && se.find(s[rightPtr]) != se.end() ){
+             se.erase(s[leftPtr]);
+            leftPtr++; 
+        }
+      se.insert(s[rightPtr]);
+      ans = max(ans, rightPtr-leftPtr);
+      rightPtr++;
+     }
+     return ans+1;
+    }
+};
+
+
+/*
+ genrate all substrings meanwhile check the  longest  substring without repeating
+    tc : O(n*n*n)
+    sc : O(26) :
+
+  approach 2: 
+  
+    traverse using two ptrs meanshiwl stroing all the chars in set , 
+
+     if char found : remove it from set  :  and also move leftptr till leftside reaching of char : 
+
+      tc :O(n) : 
+      sc : O(n)
+
+       // "a b c b b c b b"
+
+    //      a,b,c,b : 
+    //      0,1,2,3 
+
+"abcdefe" 
+"abcbbcbb"
+*/
