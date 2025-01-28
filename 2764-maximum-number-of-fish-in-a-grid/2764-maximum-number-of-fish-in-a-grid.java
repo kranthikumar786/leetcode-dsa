3ebf65e3317d8d0fc7b79164a11bb0e1aa96bfd7
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-01-29 00:47:49 +0530

    Time: 7 ms (24.93%), Space: 45 MB (57.42%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -1,36 +1,40 @@
 class Solution {
-      public boolean isRange(int x, int y , int rows, int columns) {
-          return (x >= 0  && x < rows && y >= 0 && y < columns);
-      }
+    public boolean inRange(int x, int y , int rows, int columns) {
+          return(x >= 0 && x  < rows &&  y >= 0  && y < columns);
+    }
     public int findMaxFish(int[][] grid) {
-        int rows = grid.length;
-        int columns = grid[0].length;
-        Queue<int[]>Q= new LinkedList<>();
+      int rows = grid.length;
+      int columns = grid[0].length;
+      int ans = 0 ;
+      Queue<int[]> Q = new  LinkedList<>();
       boolean visited[][] = new boolean[rows][columns];
-       int ans = 0;
-       for(int row = 0 ; row < rows ; row++) {
-          for(int column = 0 ; column < columns; column++) {
-              if(grid[row][column] > 0 && !visited[row][column]) {
-                Q.add(new int[]{row, column});
+    for(int row = 0 ; row < rows ; row++) {
+          for(int column = 0 ; column < columns ; column++) {
+             if(grid[row][column] > 0  && !visited[row][column]) {
+                 Q.add(new int[]{row,column});
                  visited[row][column] = true;
-                 int val = 0 ;
-                   while(Q.size() > 0) {
-                      int []a = Q.poll();
-                        val += grid[a[0]][a[1]]; 
-                 int dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-                  for(int i = 0 ; i < 4 ;i++) {
-                      int newX =  a[0] + dir[i][0];
-                      int newY = a[1] + dir[i][1];
-                       if(isRange(newX,newY, rows, columns) && grid[newX][newY] > 0 && !visited[newX][newY]) {
-                         Q.add(new int[]{newX, newY});
-                         visited[newX][newY] = true;
-                       }
-                  }
-                   }
-                  ans = Math.max(ans, val); 
-              }
+                   int val = 0;
+                 while(Q.size() > 0 ) {
+                   int cor[] = Q.poll();
+                    int x = cor[0];
+                    int y = cor[1];
+                    val += grid[x][y];
+                    int dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
+                   for(int i = 0 ;i < 4; i++) {
+                       int newX = x + dir[i][0];
+                       int newY = y + dir[i][1];
+                       if(inRange(newX,newY,rows,columns) && grid[newX][newY] > 0 && !visited[newX][newY]) {
+                          Q.add(new int[]{newX,newY});
+                          visited[newX][newY] = true;
+                       }  
+                   } 
+                 }
+                ans = Math.max(ans,val); 
+
+             }
+             
           }
-       }
-       return ans;
+    }
+  return ans;
     }
 }
