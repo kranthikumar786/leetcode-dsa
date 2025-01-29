77fce25aa50f8313b899b2922b4dc5836f38ff52
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-01-29 20:38:27 +0530

    Time: 15 ms (10.5%), Space: 37.1 MB (5.18%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool dfs(int parent, const vector<vector<int>>& adjList, int src, vector<int>& visited) {
-        visited[src] = 1;
-        for (int neighbor : adjList[src]) {
-            if (!visited[neighbor]) {
-                if (dfs(src, adjList, neighbor, visited))
-                    return true;
-            } else if (neighbor != parent) {
-                return true; 
-            }
-        }
-        return false;
-    }
-
+  bool dfs(int parent, int src,vector<int>& visited,vector<vector<int>>& adjList) {
+          visited[src] = 1;
+       for(auto neighbour : adjList[src]) {
+           if(!visited[neighbour]) {
+              if(dfs(src,neighbour,visited,adjList)){
+                 return 1;
+              }
+           } else if(parent != neighbour){
+                    return 1;
+           }
+       }
+        return 0; 
+  }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<vector<int>> adjList(n + 1); 
-        vector<int> removedEdge;
-
-        for (auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
-            vector<int> visited(n + 1, 0);
-            if (dfs(-1, adjList, u, visited)) {
-                removedEdge = {u, v};
-                break;
-            }
-        }
-        return removedEdge;
+     int n = edges.size();
+      vector<vector<int>>adjList(n+1);
+      vector<int>removedEdge;
+       for(auto& edge  : edges){
+          int u = edge[0], v = edge[1];
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+         vector<int>visited(n+1,0);
+          if(dfs(-1,u,visited,adjList)){
+             removedEdge = {u,v};
+             break;
+          } 
+       }
+    return removedEdge;
     }
 };
