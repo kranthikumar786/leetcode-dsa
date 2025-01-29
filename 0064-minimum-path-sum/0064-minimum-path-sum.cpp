88b9340523e54c15b329c204cb78e053fea9a476
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-01-30 02:10:09 +0530

    Time: 3 ms (31.5%), Space: 16.2 MB (70.02%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
    bool inRange(int row, int column, int rows, int columns){
-      return (row >= 0 && row < rows && column >= 0 && column < columns);
+       return (row >= 0 && row < rows && column >= 0 && column < columns);
    }
-  int gSum = INT_MAX;
-    int dfs(int x, int y , vector<vector<int>>& grid ,int rows, int columns,vector<vector<int>>&DP) {
-        if(!inRange(x,y,rows,columns)){
-            return INT_MAX;
-        }
-         if(DP[x][y] != -1){
-            return DP[x][y];
-         }
-        if(x == rows-1&& y == columns-1){
-              return grid[x][y];
-           }
-         int downSide = dfs(x+1,y,grid,rows,columns,DP); 
-           int rightSide = dfs(x,y+1,grid,rows,columns,DP);
-      return DP[x][y] = min(downSide, rightSide) + grid[x][y]; 
-    } 
-    int minPathSum(vector<vector<int>>& grid) {
-      int rows = grid.size();
-      int columns = grid[0].size();
-    vector<vector<int>> DP(rows+1, vector<int>(columns+1, -1));
-      return dfs(0,0,grid,rows,columns,DP);
-    // return gSum;
+  int dfs(int row, int column,int rows,int columns,vector<vector<int>>& grid,vector<vector<int>>&DP){
+       if(!inRange(row,column,rows,columns)){
+          return INT_MAX;
+       }
+      if(row == rows -1 && column == columns-1) {
+          return grid[row][column];
+      }
+      if(DP[row][column] != -1) {
+          return DP[row][column];
+      }
+      int downSide = dfs(row+1,column,rows,columns,grid,DP);
+      int rightSide = dfs(row,column+1,rows,columns,grid,DP);
+    return DP[row][column] = min(downSide,rightSide)+grid[row][column];
+  }
+    int minPathSum(vector<vector<int>>& grid) {   
+     int rows = grid.size();
+     int columns = grid[0].size();
+     vector<vector<int>>DP(rows+1,vector<int>(columns+1,-1)); 
+    return dfs(0,0,rows,columns,grid,DP); 
     }
 };
