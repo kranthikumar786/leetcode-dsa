b665cfafc69f2e3ac7dcbe8ac957dcfc7f6d6f88
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-02-01 21:28:13 +0530

    Time: 1 ms (94.13%), Space: 43.3 MB (19.48%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+
+class Solution {
+//    public int maxDepth(TreeNode root){
+//       if(root == null){
+//          return 0;
+//       }
+//     int leftDepth =  maxDepth(root.left);
+//     int rightDepth =  maxDepth(root.right);
+//    return Math.max(leftDepth , rightDepth) +1;  
+//    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+       List<List<Integer>> ans = new ArrayList<>(); 
+        if( root == null){
+            return ans;
+        }
+    //   int depthOfTree = maxDepth(root);
+    //   for(int i = 0 ; i <= depthOfTree; i++){
+    //     ans.add(new ArrayList<>());
+    //   }
+    Queue<TreeNode> Q = new LinkedList<>();
+    Q.add(root);
+    while (Q.size() > 0){
+      int levelSize = Q.size();
+         List<Integer> ele = new ArrayList<>();
+    for(int i = 0 ; i < levelSize ; i++) {
+          TreeNode currentNode = Q.poll();
+            if(currentNode == null)
+                continue;
+            ele.add(currentNode.val);
+           if(currentNode.left != null)
+           Q.add(currentNode.left);
+          if(currentNode.right != null)
+          Q.add(currentNode.right);   
+    }
+    ans.add(0, ele);
+//   ans[depthOfTree].add(ele);
+//   depthOfTree--;
+    }
+     return ans;
+    }
+}
