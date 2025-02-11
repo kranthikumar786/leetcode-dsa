82e5a88ca645117cd1057552d22b1a86f7460717
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-02-11 12:58:06 +0530

    Time: 8 ms (30.12%), Space: 44.3 MB (42.68%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,5 +1,16 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+        return s;
+    }
+}
+
+
+
+/*class Solution {
+    public String removeOccurrences(String s, String part) {
         
         StringBuilder str = new StringBuilder();
         int sl = s.length();
@@ -14,6 +25,7 @@ class Solution {
        return str.toString(); 
    }
 }
+*/
 
         /**
 
@@ -63,5 +75,24 @@ s = aaaaabcbcbcbcbc :
          tc  :O(n^2) :  10^3 * 10^3 = 10^6 > 10^5 so TLE can be easily expected : 
          sc : O(n) :  
         abc 
+
+
+----------------------------------------
+
+   why stack is not best approach  ...? interms of implmentaion ..? 
+     we can only check the vtop valuesusing peek() but at worst case we might need to have to check 
+     10^3 size substring and at the end which might not be same last char from stack  so it kind of not best practice to write that code 
+     so simply use strinBuilder as internally it will take care about all and we can move ahead with our core / required logic : 
+
+
+pattern matching algo like KMP or other won't works here 
+becuase of removing nature and after rmeve again can form the substring : 
+  so won't work : 
+
+
+
+
+
+
          */
  
