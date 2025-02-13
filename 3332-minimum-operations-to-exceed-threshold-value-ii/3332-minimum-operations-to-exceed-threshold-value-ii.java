db6264d457edc684b881ba793b554529e1b953e5
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-02-13 12:18:14 +0530

    Time: 124 ms (96.71%), Space: 69.1 MB (95.06%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -2,15 +2,25 @@ class Solution {
     public int minOperations(int[] nums, int k) {
      PriorityQueue<Integer> minHeap = new PriorityQueue<>();   
      for(int num : nums){
+        if(num < k)
         minHeap.add(num);
      }
      int count = 0 ;
-     while (minHeap.peek() < k) {
+     while (minHeap.size() > 0 && minHeap.peek() < k) {
+        count++;
         int firstMin = minHeap.poll(); 
+        if(minHeap.size() == 0){
+            break;
+        }
         int secondMin = minHeap.poll();
-        minHeap.add(firstMin > (Integer.MAX_VALUE - secondMin)/2 ? k : ((firstMin *2) + secondMin));
-      count++;
+        int res = firstMin > (Integer.MAX_VALUE - secondMin)/2 ? k : ((firstMin *2) + secondMin);
+       if(res < k)
+        minHeap.add(res);
      } 
     return count; 
     }
 }
+/**
+tc : O(NlogN)
+sc : O(N)
+ */
