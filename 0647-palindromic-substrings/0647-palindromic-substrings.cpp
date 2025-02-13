59a8d5ed06e1ecd764adae8f3a200cc508432219
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-02-14 00:32:56 +0530

    Time: 2 ms (93.05%), Space: 8.6 MB (82.59%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int e=-1,m=-1,r,l,k,c=0;
-        string ps = "#";
-        for(int i=0;i<s.size();i++){
-            ps+=s[i]; ps+="#";  //#a#b#c#b#a
-        }
-        vector<int>v(ps.size()+1,0);
-        
-        for(int i=0;i<ps.size();i++){
-            int mr = 2*m-i, k = 0;
-            // power of precomputation
-            if(e>=i) k = min(e-i,v[mr]);
-            l = i-k; r = i+k;
-            // normal matching
-            while(--l>=0&&++r<ps.size() && ps[l]==ps[r])
-                k++;
-            l++;r--;
-            v[i] = k;
-            if(r>=e){
-                e=r;
-                m=i;
-            }
-            c+=(k+1)/2;
-        }
-        return c;
+    int countSubstrings(string A) {
+      int ans = 0 ; int n = A.length();
+   for(int i = 0 ; i < n ; i++) {
+          ans += 1;
+          int left = i-1,right = i+1;
+          while(left >= 0 && right < n) {
+              if(A[left] != A[right]) {
+                  break;
+              }    
+              left--;
+              right++;
+              ans +=1;
+          }
+           left = i , right = i+1;
+           while(left >= 0 && right < n) {
+                 if(A[left] != A[right]) {
+                  break;
+              }    
+              left--;
+              right++;
+              ans +=1;
+           }
+   }
+  return ans; 
     }
 
 };
