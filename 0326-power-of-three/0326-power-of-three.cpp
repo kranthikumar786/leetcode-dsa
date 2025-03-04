5c610a530919e37d12a273755623bca1b82cf5ec
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-03-04 18:59:56 +0530

    Time: 0 ms (100%), Space: 8.8 MB (77.77%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-   if(n == 1){return true;}
-       if(n <= 0  || n%3 != 0) {return false;}
-       while(n > 1) {
-         if(n%3 != 0) {
-            return false;
-         }
-         n /=3;
-       }
-return true;
+        return (n > 0 && 1162261467 % n == 0);
+// the largest power of three within 32 bits signed interger is 3 ^ 19 = 1162261467  
+
+//    if(n == 1){return true;}
+//        if(n <= 0  || n%3 != 0) {return false;}
+//        while(n > 1) {
+//          if(n%3 != 0) {
+//             return false;
+//          }
+//          n /=3;
+//        }
+// return true;
         
         
         /*
