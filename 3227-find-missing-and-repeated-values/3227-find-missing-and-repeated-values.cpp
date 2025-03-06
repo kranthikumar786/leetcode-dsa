677d5c91c8201c8e88e5495443b2732fbfc5b405
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-03-06 22:27:58 +0530

    Time: 3 ms (77.46%), Space: 25.6 MB (91.87%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
+    #define SIZE 40  // 40 * 64 = 2560 bits (enough for 2500)
+    long long bits[SIZE] = {0};      
+    int duplicate = -1, missing = -1;
+
+    void setNumber(int n) {
+        int index = (n - 1) / 64;
+        int pos = (n - 1) % 64;
+        
+        if (bits[index] & (1LL << pos)) {
+            duplicate = n;  
+        } else {
+            bits[index] |= (1LL << pos);  
+        }
+    }
+
+    bool isSet(int n) {
+        return bits[(n - 1) / 64] & (1LL << ((n - 1) % 64));
+    }
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<int> count(n * n + 1, 0);  
-        int missing = -1, duplicate = -1;
-
+        int range = n * n;
+        memset(bits, 0, sizeof(bits)); 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (count[grid[i][j]] > 0) {
-                    duplicate = grid[i][j];  
-                }
-                count[grid[i][j]]++; 
+                setNumber(grid[i][j]);
             }
-        } 
-        
-        for (int i = 1; i <= n * n; i++) {  
-            if (count[i] == 0) {
+        }
+
+        for (int i = 1; i <= range; i++) {
+            if (!isSet(i)) {
                 missing = i;
                 break;
             }
         }
 
-        return {duplicate, missing};  
+        return {duplicate, missing};
     }
 };
