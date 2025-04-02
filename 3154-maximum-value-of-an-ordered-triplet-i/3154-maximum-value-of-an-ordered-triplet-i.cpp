d06e132913d452d18b1b91205590a48219b43f11
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-04-02 17:03:36 +0530

    Time: 4 ms (16.57%), Space: 22.7 MB (12.05%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -4,11 +4,24 @@ public:
     long long ans = 0;
     int n = nums.size();
        if(n < 3 ) {return 0;}
-       //vector<int>maxValueTill(n,-1);
-        //maxValueTill[n-1] = nums[n-1];
-    //   for(int k = n -2; k >= 2 ; k--) {
-    //          maxValueTill[k] = max(maxValueTill[k+1], nums[k]);                 
-    //     }
+       vector<int>maxValueRightSide(n,-1);
+        maxValueRightSide[n-1] = nums[n-1];
+      for(int k = n-2; k >= 0 ; k--) {
+             maxValueRightSide[k] = max(maxValueRightSide[k+1], nums[k]);                 
+        }
+        vector<int>maxValueLeftSide(n,-1);
+       maxValueLeftSide[0] = nums[0];
+       for(int  i = 1; i < n ; i++) {
+         maxValueLeftSide[i] = max(maxValueLeftSide[i-1],nums[i]); 
+       }
+    
+    for(int i = 1; i < n -1; i++) {
+        long long aa = (long long)(maxValueLeftSide[i-1]-nums[i])*(maxValueRightSide[i+1]);
+            ans = max(ans,aa);
+    }
+
+         return ans <= 0 ? 0 :ans;
+
 
     // for(int i = 0 ;i < n-2 ; i++) {
     //       for(int j = i +1; j < n-1  ;j++ ) {
@@ -17,27 +30,28 @@ public:
     //            }
     //       } 
     //  return ans <= 0 ? 0 : ans; 
+//========================================
+    //   vector<int>maxValueLeftSide(n,-1);
+    //    maxValueLeftSide[0] = nums[0];
+    //    for(int  i = 1; i < n ; i++) {
+    //      maxValueLeftSide[i] = max(maxValueLeftSide[i-1],nums[i]); 
+    //      cout<<maxValueLeftSide[i]<<" ";
+    //    }
+    //    cout<<"\n";
+    //     for(int i = 1; i < n - 1 ;i++) {
 
-      vector<int>maxValueLeftSide(n,-1);
-       maxValueLeftSide[0] = nums[0];
-       for(int  i = 1; i < n ; i++) {
-         maxValueLeftSide[i] = max(maxValueLeftSide[i-1],nums[i]); 
-         cout<<maxValueLeftSide[i]<<" ";
-       }
-       cout<<"\n";
-        for(int i = 1; i < n - 1 ;i++) {
-
-             for(int j = i+1 ; j < n ; j++) {
+    //          for(int j = i+1 ; j < n ; j++) {
                    
-                   long long aa = (long long)(maxValueLeftSide[i-1]-nums[i])*(nums[j]);
-                if(i == 2){
+    //                long long aa = (long long)(maxValueLeftSide[i-1]-nums[i])*(nums[j]);
+    //             if(i == 2){
                   
-                    cout<<maxValueLeftSide[i-1]<<" :" << nums[i] <<" ::" <<nums[j]<<" :::\n";
-                //  cout<<aa<<" ";
-                  }
-                  ans = max(ans,aa);  
-             }
-        }
-         return ans <= 0 ? 0 :ans; 
+    //                 cout<<maxValueLeftSide[i-1]<<" :" << nums[i] <<" ::" <<nums[j]<<" :::\n";
+    //             //  cout<<aa<<" ";
+    //               }
+    //               ans = max(ans,aa);  
+    //          }
+    //     }
+
+//         return ans <= 0 ? 0 :ans; 
     }
 };
