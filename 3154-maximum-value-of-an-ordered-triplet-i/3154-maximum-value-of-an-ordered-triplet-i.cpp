90361ab6dcd8c8470976504b9f7aa9aad5a37565
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-04-02 16:57:36 +0530

    Time: 47 ms (6.02%), Space: 22.5 MB (21.69%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-
     long long ans = 0;
     int n = nums.size();
        if(n < 3 ) {return 0;}
-       vector<int>maxValueTill(n,-1);
-         maxValueTill[n-1] = nums[n-1];
-      for(int k = n -2; k >= 2 ; k--) {
-             maxValueTill[k] = max(maxValueTill[k+1], nums[k]);                 
+       //vector<int>maxValueTill(n,-1);
+        //maxValueTill[n-1] = nums[n-1];
+    //   for(int k = n -2; k >= 2 ; k--) {
+    //          maxValueTill[k] = max(maxValueTill[k+1], nums[k]);                 
+    //     }
+
+    // for(int i = 0 ;i < n-2 ; i++) {
+    //       for(int j = i +1; j < n-1  ;j++ ) {
+    //             long long aa = (long long)(nums[i]-nums[j])*(maxValueTill[j+1]);
+    //               ans = max(ans,aa);
+    //            }
+    //       } 
+    //  return ans <= 0 ? 0 : ans; 
+
+      vector<int>maxValueLeftSide(n,-1);
+       maxValueLeftSide[0] = nums[0];
+       for(int  i = 1; i < n ; i++) {
+         maxValueLeftSide[i] = max(maxValueLeftSide[i-1],nums[i]); 
+         cout<<maxValueLeftSide[i]<<" ";
+       }
+       cout<<"\n";
+        for(int i = 1; i < n - 1 ;i++) {
+
+             for(int j = i+1 ; j < n ; j++) {
+                   
+                   long long aa = (long long)(maxValueLeftSide[i-1]-nums[i])*(nums[j]);
+                if(i == 2){
+                  
+                    cout<<maxValueLeftSide[i-1]<<" :" << nums[i] <<" ::" <<nums[j]<<" :::\n";
+                //  cout<<aa<<" ";
+                  }
+                  ans = max(ans,aa);  
+             }
         }
-    for(int i = 0 ;i < n-2 ; i++) {
-          for(int j = i +1; j < n-1  ;j++ ) {
-                long long aa = (long long)(nums[i]-nums[j])*(maxValueTill[j+1]);
-                  ans = max(ans,aa);
-               }
-          } 
-     return ans <= 0 ? 0 : ans;  
+         return ans <= 0 ? 0 :ans; 
     }
 };
