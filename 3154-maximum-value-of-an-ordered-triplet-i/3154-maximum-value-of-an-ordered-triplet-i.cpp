22751268f89d5087fb55cb22df84c361e54a181c
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-04-02 17:23:28 +0530

    Time: 0 ms (100%), Space: 21.7 MB (35.84%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -4,24 +4,27 @@ public:
     long long ans = 0;
     int n = nums.size();
        if(n < 3 ) {return 0;}
-       vector<int>maxValueRightSide(n,-1);
-        maxValueRightSide[n-1] = nums[n-1];
-      for(int k = n-2; k >= 0 ; k--) {
-             maxValueRightSide[k] = max(maxValueRightSide[k+1], nums[k]);                 
-        }
-        vector<int>maxValueLeftSide(n,-1);
-       maxValueLeftSide[0] = nums[0];
-       for(int  i = 1; i < n ; i++) {
-         maxValueLeftSide[i] = max(maxValueLeftSide[i-1],nums[i]); 
-       }
+
+    int leftMax = nums[0];
+    int rightMax = 0;   
+    //    vector<int>maxValueRightSide(n,-1);
+    //     maxValueRightSide[n-1] = nums[n-1];
+    //   for(int k = n-2; k >= 0 ; k--) {
+    //          maxValueRightSide[k] = max(maxValueRightSide[k+1], nums[k]);                 
+    //     }
+    //     vector<int>maxValueLeftSide(n,-1);
+    //    maxValueLeftSide[0] = nums[0];
+    //    for(int  i = 1; i < n ; i++) {
+    //      maxValueLeftSide[i] = max(maxValueLeftSide[i-1],nums[i]); 
+    //    }
     
     for(int i = 1; i < n -1; i++) {
-        long long aa = (long long)(maxValueLeftSide[i-1]-nums[i])*(maxValueRightSide[i+1]);
-            ans = max(ans,aa);
+                   
+        rightMax = max(rightMax, leftMax - nums[i]); // Update rightMax as max diff
+            ans = max(ans, (long long)rightMax * nums[i + 1]);      // Compute max triplet value
+        leftMax = max((long long)leftMax, (long long)nums[i]);  
     }
-
          return ans <= 0 ? 0 :ans;
-
 
     // for(int i = 0 ;i < n-2 ; i++) {
     //       for(int j = i +1; j < n-1  ;j++ ) {
