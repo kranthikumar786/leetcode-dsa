f687975199106900b222022cd0dd7e81d5ce42ca
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-04-09 22:20:03 +0530

    Time: 12 ms (55.35%), Space: 31.3 MB (78.62%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,13 +1,19 @@
 class Solution {
-    public:
-        int minOperations(vector<int>& nums, int k) {
-            int mini = *min_element(nums.begin(), nums.end());
-            if (mini < k) return -1;
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int mini = *min_element(nums.begin(), nums.end());
+        if (mini < k) return -1;
 
-            bitset<100001> st;
-            for (int i : nums) 
-                if (i > k) st.set(i);
-                
-            return st.count();
+        bool seen[101] = {};
+        for (int num : nums) {
+            if (num > k) seen[num] = true;
         }
-    };
+
+        int count = 0;
+        for (int i = k + 1; i <= 100; ++i) {
+            if (seen[i]) ++count;
+        }
+
+        return count;
+    }
+};
