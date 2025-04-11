89531f3ca2b35cfb6f55515c42e735833f5c370b
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-04-11 11:33:51 +0530

    Time: 611 ms (5.14%), Space: 130.4 MB (8.48%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
+    vector<int> numberConvertingToDigits(int num) {
+        vector<int> digits;
+        while (num) {
+            digits.push_back(num % 10);
+            num /= 10;  
+        }
+        return digits;
+    }
+
+    bool leftSumAndRightSum(vector<int>& digits) {
+        int leftPtr = 0;
+        int rightPtr = digits.size() - 1;  
+        int leftSum = 0;
+        int rightSum = 0;
+        while (leftPtr < rightPtr) {
+            leftSum += digits[leftPtr++];
+            rightSum += digits[rightPtr--];
+        }
+        return leftSum == rightSum;
+    }
+
+    bool isSymmetric(int num) {
+        vector<int> digits = numberConvertingToDigits(num);
+        return leftSumAndRightSum(digits);
+    }
+
     int countSymmetricIntegers(int low, int high) {
-        int ans1 = 0 ;
-           int digits = to_string(low).length();
-            digits *= 10;
-          while(low <= high) {
-            string s = to_string(low) ;
-               if(s.length()%2 == 1){
-                 int nextLength = s.length() + 1;
+        int ans1 = 0;
+        while (low <= high) {
+            string s = to_string(low);
+            if (s.length() % 2 == 1) {
+                int nextLength = s.length() + 1;
                 low = pow(10, nextLength - 1);
-                      continue;   
-               }else{
-                      vector<int>ans;  
-                      int num = low;
-                        while(num){
-                            ans.push_back(num%10);
-                            num /= 10;
-                        }
-                    int leftPtr = 0, rightPtr = ans.size()-1;
-                     int leftSum = 0, rightSum = 0;  
-                       while(leftPtr < rightPtr) {
-                           leftSum += ans[leftPtr++];
-                           rightSum += ans[rightPtr--];
-                       }  
-                       if(leftSum == rightSum) {
-                        ans1++;
-                       }
-               low++;
-               }
-          }
-  return ans1;
+                continue;
+            }
+            if (isSymmetric(low)) {
+                ans1++;
+            }
+            low++;  
+        }
+        return ans1;
     }
 };
