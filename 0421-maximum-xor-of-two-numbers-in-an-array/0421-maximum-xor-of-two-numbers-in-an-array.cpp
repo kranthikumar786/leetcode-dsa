c46dde4ceb5dc2a753479685ce83d5847ef79db1
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-05-02 04:19:51 +0530

    Time: 555 ms (91.7%), Space: 184.1 MB (47.76%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,49 +1,93 @@
-class Trienode{
- public:
- Trienode* children[2];
- Trienode(){
-    this->children[0]=NULL;
-    this->children[1]=NULL;
- }
-};
-class Trie{
-    public:
-    Trienode* root;
-    Trie(){
-    this->root=new Trienode();}
-    void insert(int num){
-        Trienode* curr=root;
-        for(int i=31;i>=0;i--){
-            int bit=(num >> i) & 1;
-            if(curr->children[bit]==NULL){
-                curr->children[bit]=new Trienode();
-            }
-            curr=curr->children[bit];
-        }}
-    int getmax(int num){
-        Trienode* curr=root;
-        int ans=0;
-        for(int i=31;i>=0;i--){
-            int bit=(num >> i) & 1;
-            if(curr->children[1-bit] !=NULL){
-             curr=curr->children[1-bit];
-             ans = ans | (1<<i);
-            } else {
-             curr=curr->children[bit];}
-            
-        }
-        return ans;}
-};
-
 class Solution {
-
 public:
+    struct TrieNode {
+        TrieNode* child[2];
+        TrieNode() {
+            child[0] = nullptr;
+            child[1] = nullptr;
+        }
+    };
+
+    class Trie {
+        TrieNode* root;
+
+    public:
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(int num) {
+            TrieNode* node = root;
+            for (int i = 31; i >= 0; --i) {
+                int bit = (num >> i) & 1;
+                if (!node->child[bit]) {
+                    node->child[bit] = new TrieNode();
+                }
+                node = node->child[bit];
+            }
+        }
+
+        int getMaxXor(int num) {
+            TrieNode* node = root;
+            int maxXor = 0;
+            for (int i = 31; i >= 0; --i) {
+                int bit = (num >> i) & 1;
+                int toggledBit = 1 - bit;
+                if (node->child[toggledBit]) {
+                    maxXor |= (1 << i);
+                    node = node->child[toggledBit];
+                } else {
+                    node = node->child[bit];
+                }
+            }
+            return maxXor;
+        }
+    };
+
     int findMaximumXOR(vector<int>& nums) {
         Trie trie;
-        int n=nums.size();
-        for(int i=0;i<n;i++){trie.insert(nums[i]);}
-        int ans=0;
-        for(int i=0;i<n;i++){ans=max(ans,trie.getmax(nums[i]));}
-        return ans;
+        for (int num : nums) {
+            trie.insert(num);
+        }
+
+        int maxXor = 0;
+        for (int num : nums) {
+            maxXor = max(maxXor, trie.getMaxXor(num));
+        }
+        return maxXor;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int findMaximumXOR(vector<int>& nums) {
+//      int ans = 0 ;
+//      int n = nums.size();
+//       for(int i = 0 ; i < n ;i++) {
+//            for(int j = i +1 ; j < n ;j++) {
+//                 ans = max(ans, nums[i]^nums[j]);
+//            }
+//       }
+//     return ans;
+//     }
+// };
+// // TLE
+
+
+// /*
+
+//   let's sort : 
+// 2 3 5 8 10 25 :  if right means max value and not mandary more set bits
+// 2 = 10
+
+//    prefix ...? 
+ 
+//      long suffix also not helping 
+//  1  2 3  5  8 
+// 0   5
+
+
+// */
