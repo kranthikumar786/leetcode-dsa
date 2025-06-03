679ecf1925203ac23ae6e2e3e891c27ec0ce2c8e
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-06-03 12:39:48 +0530

    Time: 0 ms (100%), Space: 29.6 MB (36.8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,36 +1,51 @@
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-         int  i = 0 ; 
-         int n = nums.size();
-        for(int i = 0 ; i < n/2 ; i++){
-               swap(nums[i],nums[n-i-1]);
-        } 
+        int n = nums.size();
+        k = k % n; 
+        reverse(nums.begin(), nums.end());
         
-        k = k%n;
-       int left = 0 ;
-       int right = k-1;
-       while(left < right ){
-          swap(nums[left],nums[right]);
-          left++;
-          right--;
-       } 
-       left = k ;
-       right = n-1;
-       while(left < right ){
-          swap(nums[left],nums[right]);
-          left++;
-          right--;
-       } 
-
-        /*
-        [1,2,3,7,5,6,4]
-
-        [1,2,3,4,5,6,7]
-         0 1 2 3 4 5 6 
-
-         7 6 5 4 3 2 1 
-         5 6 7 1 2 3 4 : 
-        */
+        reverse(nums.begin(), nums.begin() + k);
+ 
+        reverse(nums.begin() + k, nums.end());
     }
 };
+
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//          int  i = 0 ; 
+//          int n = nums.size();
+//         for(int i = 0 ; i < n/2 ; i++){
+//                swap(nums[i],nums[n-i-1]);
+//         } 
+        
+//         k = k%n;
+//        int left = 0 ;
+//        int right = k-1;
+//        while(left < right ){
+//           swap(nums[left],nums[right]);
+//           left++;
+//           right--;
+//        } 
+//        left = k ;
+//        right = n-1;
+//        while(left < right ){
+//           swap(nums[left],nums[right]);
+//           left++;
+//           right--;
+//        } 
+
+//         /*
+//         [1,2,3,7,5,6,4]
+
+//         [1,2,3,4,5,6,7]
+//          0 1 2 3 4 5 6 
+
+//          7 6 5 4 3 2 1 
+//          5 6 7 1 2 3 4 : 
+//         */
+//     }
+// };
