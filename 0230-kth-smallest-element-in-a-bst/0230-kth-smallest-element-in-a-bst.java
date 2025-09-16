00d365ef5f3bd3845ad4e7528226d3cbfecb7b3f
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-09-16 19:49:53 +0530

    Time: 0 ms (100%), Space: 44.4 MB (76.43%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,22 +14,43 @@
  * }
  */
 class Solution {
-   ArrayList<Integer> sorted = new ArrayList<>();
-   void inorderTravesal(TreeNode root) {
-       if(root != null) {
-         inorderTravesal(root.left);
-         sorted.add(root.val);
-         inorderTravesal(root.right);
-       }
-   }
-    public int kthSmallest(TreeNode root, int k) {
-          inorderTravesal(root);
-          return sorted.get(k-1);
+//    ArrayList<Integer> sorted = new ArrayList<>();
+//    void inorderTravesal(TreeNode root) {
+//        if(root != null) {
+//          inorderTravesal(root.left);
+//          sorted.add(root.val);
+//          inorderTravesal(root.right);
+//        }
+//    }
+// inorderTravesal(root);
+          //return sorted.get(k-1);
         /**
         BST inorder travesal gives the sorted order elements :
          so stor ein in array , then find the K-1 elemnte return it :
        TC: O(n)
        SC: O(n)
          **/
+     private int count;
+    private int ans;
+    
+    public int kthSmallest(TreeNode root, int k) {
+        count = 0;       // reset for each test case
+        ans = -1;
+        inOrderTraversal(root, k);
+        return ans;
+    }
+
+    private void inOrderTraversal(TreeNode root, int k) {
+        if (root == null) return;
+
+        inOrderTraversal(root.left, k);
+
+        count++;
+        if (count == k) {
+            ans = root.val;
+            return;
+        }
+
+        inOrderTraversal(root.right, k);
     }
 }
