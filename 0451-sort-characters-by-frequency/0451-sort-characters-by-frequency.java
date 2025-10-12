13bae5afa501ae47066d7f1bd1e2732af55f609a
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-10-12 19:30:14 +0530

    Time: 23 ms (14.28%), Space: 45.6 MB (76.11%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,30 +1,26 @@
 class Solution {
     public String frequencySort(String s) {
-        int n = s.length();
-        HashMap<Character, Integer> freMap = new HashMap<>();
-        HashMap<Character, Integer> charAtFirst = new HashMap<>();
+      HashMap<Character,Integer>charFre = new HashMap<>();
+      HashMap<Character,Integer>firstChar = new HashMap<>();
+      for(int i = 0 ; i < s.length() ; i++) {
+          char c = s.charAt(i);
+         int prevF = charFre.getOrDefault(c,0);
+         charFre.put(c,prevF+1);
+         firstChar.putIfAbsent(c,i);
+      }
+      List<Character>allChars = new ArrayList<>(charFre.keySet());
+      allChars.sort((a,b) ->{
+          int fre = Integer.compare(charFre.get(b),charFre.get(a));
+          if(fre != 0) return fre;
+          return Integer.compare(firstChar.get(a),firstChar.get(b)); 
+      });
 
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            freMap.put(ch, freMap.getOrDefault(ch, 0) + 1);
-            charAtFirst.putIfAbsent(ch, i);
-        }
+     StringBuilder str = new StringBuilder();
 
-        List<Character> allchars = new ArrayList<>(freMap.keySet());
-        allchars.sort((a, b) -> {
-            int fre = Integer.compare(freMap.get(b), freMap.get(a)); // sort by descending frequency
-            if (fre != 0) return fre;
-            return Integer.compare(charAtFirst.get(a), charAtFirst.get(b)); // if same frequency, earlier occurrence
-        });
-
-        StringBuilder result = new StringBuilder();
-        for (Character ch : allchars) {
-            int count = freMap.get(ch);
-            for (int i = 0; i < count; i++) {
-                result.append(ch);
-            }
-        }
-
-        return result.toString();
+      for(Character ch :  allChars){
+        int fre = charFre.get(ch);
+        str.append(String.valueOf(ch).repeat(fre));  
+      }
+      return str.toString();
     }
 }
