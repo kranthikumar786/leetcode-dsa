fd6d8d06f58bc36cc11dfd1403f28ba81386cbc5
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-10-13 14:49:42 +0530

    Time: 6 ms (41.38%), Space: 42.6 MB (99.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer>hm = new HashMap<>();
+       int arr[] = new int[26];
+       if(s.length() != t.length()) return false;
+       for(int i  = 0 ;i < s.length() ; i++){
+              arr[s.charAt(i) -'a']++;
+              arr[t.charAt(i) -'a']--;
+       }
+       for(int i = 0 ; i < 26 ; i++) {
+              if(arr[i] != 0) return false;
+       }
+       return true;
+    }
+}
+/**
+HashMap<Character,Integer>hm = new HashMap<>();
         if(s.length() != t.length()) return false;
         
         for(char ch : s.toCharArray()){
@@ -16,5 +29,4 @@ class Solution {
              }
         }
        return hm.isEmpty();  
-    }
-}
+ */
