e444ee415e6038c7fa0a56099dc0241a6c0ca49c
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-10-20 22:50:43 +0530

    Time: 0 ms (100%), Space: 42.7 MB (77.82%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -3,7 +3,8 @@ class Solution {
 
        int num = 0 ;
        for(String operation : operations) {
-           num += (operation.equals("++X") || operation.equals("X++")) ? 1 : -1; 
+           //num += (operation.equals("++X") || operation.equals("X++")) ? 1 : -1; 
+           num += (operation.charAt(1) == '+') ? 1:-1;
        }
    return num;
     }
