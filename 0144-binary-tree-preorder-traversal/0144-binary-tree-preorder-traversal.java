df57cf2d73001f4584e657440836ccafbb892162
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-07 23:00:40 +0530

    Time: 0 ms (100%), Space: 43.1 MB (6.05%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,41 +13,21 @@
  *     }
  * }
  */
-
-
-  
-
-/*
-root = [1,null,2,3]
-Output: [1,2,3]
-*/
 class Solution {
-    //   public void preOrder(TreeNode root, List<Integer>ans){
-    //       if(root == null) {return;}
-    //      ans.add(root.val);
-    //      preOrder(root.left,ans);
-    //      preOrder(root.right,ans);
-    //   }
-
-public void PreOrder(TreeNode root , List<Integer>ans){
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(st.size() > 0) {
-             TreeNode currentNode = st.peek();
-              st.pop();
-              if(currentNode == null)
-                 continue;
-              ans.add(currentNode.val);
-              if(currentNode.right != null) {
-                st.push(currentNode.right);
-              }
-              if(currentNode.left != null) {
-                st.push(currentNode.left);
-              }
-     } }
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        PreOrder(root,ans);
-        return ans;
+     List<Integer>ans = new ArrayList<>();
+       Stack<TreeNode>st = new Stack<>(); 
+        TreeNode cur = root;
+         while(cur != null || !st.isEmpty()) {
+                while(cur != null) {
+                    ans.add(cur.val);
+                    if(cur.right != null)
+                      st.push(cur.right);
+                    cur = cur.left;  
+                }
+                 if(!st.isEmpty())
+                  cur = st.pop();
+         }
+      return ans;
     }
 }
