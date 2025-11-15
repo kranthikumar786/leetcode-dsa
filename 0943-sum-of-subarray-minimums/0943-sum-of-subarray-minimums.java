c736760d7a4cf3da0c0bf7dd49f702d0359c8e7b
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-16 00:56:45 +0530

    Time: 62 ms (68.31%), Space: 59.5 MB (5.08%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -3,14 +3,40 @@ class Solution {
     int n = arr.length;
     long  sum  = 0;
     int mod = 1_000_000_007;
-   for(int i = 0 ; i < n ;i++) {
+    int leftC[] = new int[n];
+    int rightC[] = new int[n];
+    Stack<Integer> st = new Stack<>();
+    for(int i = 0 ; i < n ; i++) {
+           while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                 st.pop();
+           }
+           int dist = st.isEmpty() ? i+1 :i-st.peek();
+           leftC[i] = dist;
+           st.push(i);  
+    }
+    st.clear();
+
+  for(int i = n-1; i >= 0 ;i--) {
+          while(!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+          }
+          int dist = st.isEmpty() ? (n-i) : st.peek()-i;
+          rightC[i] = dist;
+          st.push(i); 
+  } 
+  for(int  i = 0 ; i < n ;i++) {
+       long contrib = ((long) arr[i] * (long) leftC[i] * (long) rightC[i]) % mod;
+            sum = (sum + contrib) % mod;
+  } 
+    /**
+
+    for(int i = 0 ; i < n ;i++) {
        int minValue = arr[i] ;
        for(int j = i ; j < n ; j++) {
              minValue = Math.min(minValue,arr[j]);
              sum = (sum +minValue)%mod;
        }
    }
-    /**
      3 1 2 4 :
       3 =>3
       3 1 =>1
@@ -22,7 +48,6 @@ class Solution {
       2 = >2 :
        2, 4 => 2: 
        4 = > 4: 
-
      */
    return (int)sum;
     }
