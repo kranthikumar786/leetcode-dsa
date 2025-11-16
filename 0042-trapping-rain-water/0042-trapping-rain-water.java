faee8715cb1e9c8d9e7b7d6866f72dba1127c4cd
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-16 13:07:50 +0530

    Time: 0 ms (100%), Space: 47.9 MB (8.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,6 +2,45 @@ class Solution {
     public int trap(int[] height) {
         int  n  = height.length;
         int water = 0;
+        int leftMax =  0 , rightMax = 0;
+        int leftPtr = 0 , rightPtr = n-1;
+         while(leftPtr <= rightPtr ) {
+              if(height[leftPtr] <= height[rightPtr]) {
+                    if(height[leftPtr]  >= leftMax) {
+                        leftMax = height[leftPtr];
+                    } else{
+                        water += (leftMax - height[leftPtr]);
+                    }
+                 leftPtr++;   
+              } else{
+                  if (height[rightPtr] >= rightMax) {
+                    rightMax = height[rightPtr];
+                } else {
+                    water += rightMax - height[rightPtr];
+                }
+                rightPtr--;      
+              }
+         }
+    return water;     
+    }
+}
+
+/**
+
+
+int rightMax[] =  new int[n];
+        
+         rightMax[n-1] = height[n-1];
+         for(int i = n-2 ; i >= 0;i--) {
+            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+         }
+
+since array traversing from left , just need of leftMax , irresrpcive of subarrya
+  so metaining simply leftMax help same for rightMax as welll if traveser from right side:
+
+
+  int  n  = height.length;
+        int water = 0;
         int rightMax[] =  new int[n];
         
          rightMax[n-1] = height[n-1];
@@ -13,13 +52,8 @@ class Solution {
             leftMax = Math.max(leftMax,height[i]);    
             water += Math.min(leftMax,rightMax[i]) - height[i];
         } 
-    return water;     
-    }
-}
-
-/**
-
-
+    return water;
+=====================================================
 int leftMax[] = new int[n];
         int rightMax[] =  new int[n];
         leftMax[0] = height[0];
