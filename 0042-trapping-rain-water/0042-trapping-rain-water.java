91d1924830a91bc9825081334c6a35eacd785212
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-16 12:40:10 +0530

    Time: 1 ms (57.77%), Space: 48.8 MB (6.07%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int trap(int[] height) {
+        int  n  = height.length;
+        int water = 0;
+        int leftMax[] = new int[n];
+        int rightMax[] =  new int[n];
+        leftMax[0] = height[0];
+        for(int i = 1 ; i < n ;i++){
+            leftMax[i] = Math.max(leftMax[i-1],height[i]); 
+        }
+         rightMax[n-1] = height[n-1];
+         for(int i = n-2 ; i >= 0;i--) {
+            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+         }
+        for(int i = 0 ; i < n ;i++) {
+            water += Math.min(leftMax[i],rightMax[i]) - height[i];
+        } 
+    return water;     
+    }
+}
+
+/**
+
+    int leftMax = 0;
+             for(int j = 0 ; j <=i; j++){
+                leftMax = Math.max(leftMax,height[j]);
+             }
+             int rightMax = 0;
+             for(int k = i; k < n ;k++) {
+                rightMax = Math.max(rightMax,height[k]);
+             }
+           water += Math.min(leftMax,rightMax) - height[i];
+
+
+
+TLE 
+
+int  n =height.length;
+    int water = 0 ;
+    for(int i = 0 ; i < n ;i++) {
+           int max1 = 0;
+           boolean f = false;
+           for(int j = i+1 ; j < n ;j++) {
+                  if(height[i] >= height[j]){
+                      max1 +=(height[i]-height[j]);
+                       f = true;
+                  }else{
+                    break;
+                  }
+           }
+           if(f)
+           water = Math.max(water,max1);
+    }
+
+*/
