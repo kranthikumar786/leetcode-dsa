a8c684ace9360462419659d8d848936947fba605
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-18 00:17:14 +0530

    Time: 17 ms (38.52%), Space: 47.9 MB (20.09%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,8 +1,73 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+       int n = nums.length;
+       int ans[] = new int[n];
+       Stack<Integer>st = new Stack<>();
+       for(int i = 2*n-1; i >= 0 ; i--) {
+           int idx = i%n;
+           while(!st.isEmpty() && nums[st.peek()] <= nums[idx]){
+                 st.pop();
+           }
+           if(idx < n){
+              ans[idx] = st.isEmpty() ? -1: nums[st.peek()];
+           }
+         st.push(idx);   
+       }
+      return ans; 
+    }
+}
+
+/**
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
       int n = nums.length;
       int ans[] = new int[n];
-      for(int i = 0 ; i < n ;i++) {
+      Stack<Integer>st = new Stack<>();
+      for(int i = 0 ; i < n ; i++) {
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+                  st.pop();
+            }   
+          st.push(i);  
+      }
+      for(int  i = n-1; i >= 0 ;i--) {
+           while(!st.isEmpty() && nums[st.peek()] > nums[i]){
+                st.pop();
+           }
+         ans[i] = st.isEmpty() ? -1 ; nums[st.peek()];
+         st.push(i);  
+         }     
+      }
+     return ans; 
+    }
+}
+
+
+/**
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+      int n = nums.length;
+      int ans[] = new int[n];
+      Stack<Integer>st = new Stack<>();
+      for(int i = 0 ; i < n ; i++) {
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+                  st.pop();
+            }   
+          st.push(i);  
+      }
+      for(int  i = n-1; i >= 0 ;i--) {
+           while(!st.isEmpty() && nums[st.peek()] > nums[i]){
+                st.pop();
+           }
+         ans[i] = st.isEmpty() ? -1 ; nums[st.peek()];
+         st.push(i);  
+         }     
+      }
+     return ans; 
+    }
+}
+
+for(int i = 0 ; i < n ;i++) {
            int nextMax = -1;
            for(int j = i+1; j < n*2; j++) {
                 int idx = j%n;
@@ -13,14 +78,8 @@ class Solution {
                           break;
                      }  
                 }
-        ans[i] = nextMax;    
-      }
-     return ans; 
-    }
-}
+        ans[i] = nextMax;
 
-
-/**
 INput: 
 [1,8,-1,-100,-1,222,1111111,-111111]
 
