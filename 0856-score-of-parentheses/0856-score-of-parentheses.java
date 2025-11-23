7cff97faaff91e7f6b487413368e6854ce2bb6f5
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-23 20:02:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (19.63%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,19 +1,20 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        int Score = 0;
-        int depth = 0; 
+        //int Score = 0;
+        //int depth = 0; 
         int n = s.length();
+        ArrayDeque<Integer>st = new ArrayDeque<>();
+        st.push(0);
         for(int i = 0 ; i < n ; i++){
-              if(s.charAt(i) == '('){
-                depth++;
-              } else{
-                 depth--;
-                 if(s.charAt(i-1) == '('){
-                    Score += 1<<depth;
-                 }
-              }
+           if(s.charAt(i) == '('){
+                st.push(0);
+           } else{
+               int inner = st.pop();
+               int v = inner == 0 ? 1 : inner*2; 
+               st.push(st.pop()+v);
+           }
         }
-        return Score;
+        return st.pop();
     }
 }
 
