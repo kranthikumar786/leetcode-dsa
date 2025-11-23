eec63467c72586734763fa0068861701f38dbead
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-11-23 15:41:10 +0530

    Time: 2 ms (97.36%), Space: 44.5 MB (85.69%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,22 +1,48 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer>st = new Stack<>();
-        int ans = 0 ;
-        int n = s.length();
-        st.push(-1);
-        for(int i = 0 ; i < n ; i++) {
-            char ch = s.charAt(i);
-            if(ch == '(') {
-                 st.push(i);
-            } else{
-                st.pop();
-                if(st.isEmpty()) {
-                    st.push(i);
-                } else{
-                   ans = Math.max(ans,i-st.peek()); 
-                }
-            }
+        int left = 0, right = 0, maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') left++;
+            else right++;
+            if (left == right) maxLen = Math.max(maxLen, 2 * right);
+            else if (right > left) left = right = 0;
         }
-       return ans;  
+
+        left = right = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') left++;
+            else right++;
+            if (left == right) maxLen = Math.max(maxLen, 2 * left);
+            else if (left > right) left = right = 0;
+        }
+
+        return maxLen;
     }
 }
+
+
+
+// class Solution {
+//     public int longestValidParentheses(String s) {
+//         Stack<Integer>st = new Stack<>();
+//         int ans = 0 ;
+//         int n = s.length();
+//         st.push(-1);
+//         for(int i = 0 ; i < n ; i++) {
+//             char ch = s.charAt(i);
+//             if(ch == '(') {
+//                  st.push(i);
+//             } else{
+//                 st.pop();
+//                 if(st.isEmpty()) {
+//                     st.push(i);
+//                 } else{
+//                    ans = Math.max(ans,i-st.peek()); 
+//                 }
+//             }
+//         }
+//        return ans;  
+//     }
+// }
