a3ffa45aac7e4d587fa5dec80af36401f0f3781c
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-12-02 17:18:56 +0530

    Time: 5 ms (37.12%), Space: 46.6 MB (33.72%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,10 +9,37 @@
  * }
  */
 class Solution {
+     class Pair{
+        int val;
+        ListNode node;
+        Pair(int val,ListNode node){
+            this.val = val;
+            this.node = node;
+        }
+     }  
     public ListNode mergeKLists(ListNode[] lists) {
-        
-        int n = lists.length;
-        // ArrayList<Integer>arr = new ArrayList<>();
+       PriorityQueue<Pair>minHeap = new PriorityQueue<>((a,b) ->(a.val-b.val));
+       ListNode dummy = new ListNode(-1);
+       ListNode tail = dummy;
+       int n = lists.length;
+       for(int i = 0 ; i < n  ; i++){
+            if(lists[i] != null)
+             minHeap.add(new Pair(lists[i].val,lists[i]));
+       }
+       while(minHeap.size() > 0) {
+         Pair p = minHeap.poll();
+         tail.next = p.node;
+         if(p.node.next != null){
+            minHeap.add(new Pair(p.node.next.val,p.node.next));
+         } 
+          tail = tail.next;
+       } 
+    return dummy.next;
+        /**
+         [1,4,5]
+         [1,3,4]
+         [2,6]
+// ArrayList<Integer>arr = new ArrayList<>();
         PriorityQueue<Integer>minHeap = new PriorityQueue<>();
         for(int i = 0; i < n ;i++) {
               ListNode node = lists[i]; 
@@ -36,13 +63,37 @@ class Solution {
            }
       return head;
 
-        /**
-         [1,4,5]
-         [1,3,4]
-         [2,6]
-
        //  create an array of totalNo.of elemts:
           then sort in ascending orde rthen , craetar an linkedlist 
+         ===================================================
+        int n = lists.length;
+        ListNode head = null;
+        ListNode l1 = lists[0];
+        ListNode prev = new ListNode(-1);
+        for(int i = 1; i < n ; i++) {
+            ListNode l2 = lists[i];
+             while(l1 != null && l2 != null) {
+                    if(l1.val <= l2.val){
+                         if(head == null) {
+                            head = l1;
+                         }
+                      prev.next = l1;
+                      prev = l1;
+                      l1 = l1.next; 
+                    } else{
+                        prev.next = l2;
+                        prev = l2;
+                        l2 = l2.next;
+                    }
+             }
+             while(l2 != null ){
+               prev.next = 
+                l1.next = l2;
+                l2 = l2.next;
+                l1 = l1.next;
+                
+             }
+        }
          */
 
     }
