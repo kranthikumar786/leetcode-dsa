2d628f6fa9461cce87b81e33644d3d313c2704b9
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-12-03 17:10:30 +0530

    Time: 5 ms (77.77%), Space: 77.4 MB (5.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,29 @@
 class Solution {
     public int maxArea(int[] height) {
-      int rightPtr = height.length-1;
-      int leftPtr = 0;
-      int maxWater = 0;
-      while(leftPtr < rightPtr) {
-        int minValue = Math.min(height[leftPtr],height[rightPtr]);
-        maxWater = Math.max(maxWater, ((rightPtr-leftPtr) * minValue));
-       if(height[leftPtr] <= height[rightPtr]) {
-        leftPtr++;
-       } else{
-        rightPtr--;
-       }
-      }
-      return maxWater;
+       int ans = 0;
+        int n = height.length;
+       int l = 0 ,r = n-1;
+         while(l < r){
+            int pol = Math.min(height[l],height[r]);
+            int dist = r-l;
+            ans = Math.max(ans, dist*pol);
+           if(height[l] <= height[r]){
+            l++;
+           }else{
+            r--;
+           }
+         }
+         return ans;
+      /**
+       Approach 1: 
+            fix one pole and look for another pole in all possible ways:
+              using two loops:
+               then calucate ans:
+          TC: O(n^2):
+          SC: O(1):     
+        Approach 2: 
+          dist is fine , looking for max eelmts , so eleimate min from etiher of left or right :
+               and calc the ans:  
+       */   
     }
 }
