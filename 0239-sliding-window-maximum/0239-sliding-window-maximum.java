f07a39667d01787a6c69e2ea627864e9051a9ca7
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2025-12-21 23:34:19 +0530

    Time: 28 ms (89.88%), Space: 143.6 MB (67.57%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,21 +1,50 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-      int n = nums.length;
-      int ans[] = new int[n-k+1];
-        int index = 0;
-      Deque<Integer>dq = new LinkedList<>();
-      for(int i = 0 ; i < n ;i++) {
-          while(!dq.isEmpty() && nums[dq.getLast()] <= nums[i]) {
-                   dq.removeLast();
-          }
-          dq.addLast(i);
-            if (!dq.isEmpty() && dq.getFirst() <= i - k) {
-                dq.removeFirst();
+
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+
+        int k1 = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
             }
-          if(i >= k-1){
-            ans[index++] = nums[dq.getFirst()];
-          } 
-      } 
-      return ans;
+
+            dq.offerLast(i);
+
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            if (i >= k - 1) {
+                ans[k1++] = nums[dq.peekFirst()];
+            }
+        }
+
+        return ans;
     }
 }
+// class Solution {
+//     public int[] maxSlidingWindow(int[] nums, int k) {     
+//      int n = nums.length;
+//      int ans[] = new int[n-k+1];
+//      ArrayDeque<Integer>dq = new ArrayDeque<>();
+//      int k1 = 0 ;
+//      for(int i = 0 ; i < n ; i++) {
+//             while(!dq.isEmpty() && nums[i] >= nums[dq.back()]) {
+//                     dq.pop_back(); 
+//             }
+//              dq.push(i);
+//             if(!dq.isEmpty() && dq.front() <= i-k) {
+//                 dq.pop_front();
+//             }   
+//            if(i >= k-1){
+//             ans[k1++] = nums[dq.front()];
+//            }
+//      }
+//      return ans; 
+//     }
+// }
