9c32955dc1946ab024085ae3fcbd2b3661ec3187
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2026-01-04 16:37:58 +0530

    Time: 9 ms (97.47%), Space: 45.7 MB (98.99%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,32 +1,33 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
-    int sum = 0 ; 
+    int totalSum = 0 ;
     for(int num : nums){
-        ArrayList<Integer>arr = new ArrayList<>();
-          boolean f = false;
-        for(int i = 1 ; i*i <= num; i++) {
-               if(num%i == 0){
-                 arr.add(i);
-                  if(i != num/i){
-                    arr.add(num/i);
-                  }
+          int divisorSum = 0 ;
+          int divisiorCount = 0;
+          for(int i = 1 ; i*i <= num ; i++) {
+               if(num%i != 0 ){
+                  continue;
                }
-                if(arr.size() > 4) {
-                      f = true;
-                       break;
-                }
-        }
-        if(arr.size() == 4 && !f){
-            //sum += arr.stream().sum();
-        sum += arr.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(num +":::: "+ sum);
-        }
+               divisiorCount++;
+               divisorSum+=i;
+               int divisorPair = num/i;
+               if(i != divisorPair){
+                   divisiorCount++;
+                    divisorSum +=divisorPair;  
+               }
+                if(divisiorCount > 4){break;}
+          }
+          if(divisiorCount == 4){
+            totalSum += divisorSum;
+          }
     }
-    return sum;
+    return totalSum;
     }
 }
-
-
+/**
+TC : O(N* Square(MaxNumberinNums))
+SC: O(1)
+ */
 /**
     int sum = 0 ; 
     for(int num : nums){
