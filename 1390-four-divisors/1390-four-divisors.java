bfe4afa85f2ae247221d6bc85c2b01b51edccd4f
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2026-01-04 16:24:03 +0530

    Time: 45 ms (16.67%), Space: 47.5 MB (6.82%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -4,6 +4,34 @@ class Solution {
     for(int num : nums){
         ArrayList<Integer>arr = new ArrayList<>();
           boolean f = false;
+        for(int i = 1 ; i*i <= num; i++) {
+               if(num%i == 0){
+                 arr.add(i);
+                  if(i != num/i){
+                    arr.add(num/i);
+                  }
+               }
+                if(arr.size() > 4) {
+                      f = true;
+                       break;
+                }
+        }
+        if(arr.size() == 4 && !f){
+            //sum += arr.stream().sum();
+        sum += arr.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(num +":::: "+ sum);
+        }
+    }
+    return sum;
+    }
+}
+
+
+/**
+    int sum = 0 ; 
+    for(int num : nums){
+        ArrayList<Integer>arr = new ArrayList<>();
+          boolean f = false;
         for(int i = 1 ; i <= num; i++) {
                if(num%i == 0){
                  arr.add(i);
@@ -20,5 +48,5 @@ class Solution {
         }
     }
     return sum;
-    }
-}
+
+ */
