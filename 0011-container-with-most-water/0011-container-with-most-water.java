2ed44f50a154715b11cc64607a64198bf5805603
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2026-06-09 23:13:03 +0530

    Time: 5 ms (82.63%), Space: 77.4 MB (27.73%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,29 +1,31 @@
 class Solution {
     public int maxArea(int[] height) {
-       int ans = 0;
-        int n = height.length;
-       int l = 0 ,r = n-1;
-         while(l < r){
-            int pol = Math.min(height[l],height[r]);
-            int dist = r-l;
-            ans = Math.max(ans, dist*pol);
-           if(height[l] <= height[r]){
-            l++;
-           }else{
-            r--;
-           }
-         }
-         return ans;
-      /**
-       Approach 1: 
-            fix one pole and look for another pole in all possible ways:
-              using two loops:
-               then calucate ans:
-          TC: O(n^2):
-          SC: O(1):     
-        Approach 2: 
-          dist is fine , looking for max eelmts , so eleimate min from etiher of left or right :
-               and calc the ans:  
-       */   
+        int maxWater = 0;
+        int left = 0; 
+        int right = height.length - 1;
+        
+        while (left < right) {
+            int dist = right - left;
+            int minPoleH = Math.min(height[left], height[right]);
+            maxWater = Math.max(maxWater, (minPoleH * dist));
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }  
+        return maxWater;
     }
 }
+
+/**
+for(int l = 0 ; l < n ;l++) {
+           int ans = 0;
+            for(int r = l+1; r < n; r++) {
+                  int minV =  Math.min(height[l],height[r]) * (r-l);    
+                 ans = Math.max(minV,ans);
+            }
+            maxWater = Math.max(maxWater,ans);
+     }
+ */
