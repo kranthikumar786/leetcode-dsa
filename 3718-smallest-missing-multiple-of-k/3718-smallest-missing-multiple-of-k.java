906429f914eafc6807648fb03cb181f20410de46
KranthiKumar <53251655+kranthikumar786@users.noreply.github.com>
2026-08-25 23:03:25 +0530

    Time: 1 ms (82%), Space: 44.9 MB (83.31%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        boolean[] contains = new boolean[201];
+        for (int ele : nums) {
+            contains[ele] = true;
+        }
+        int ans = k;
+        while (contains[ans]) {
+            ans += k;
+        }
+        return ans;
+    }
+}
